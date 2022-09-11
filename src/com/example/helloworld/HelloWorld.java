5c7e0f3d42cb79c10d852eb751172ea650a5f45b
Annie Dinh <dinh.a@northeastern.edu>
2022-09-11 16:27:53 -0400

    Initial commit

---
+++ src/com/example/helloworld/HelloWorld.java
@@ -0,0 +1,8 @@
+package com.example.helloworld;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("My First Java program. Hello World");
+        System.out.println("Adding more after JAR built. I need to update JAR now");
+    }
+}
